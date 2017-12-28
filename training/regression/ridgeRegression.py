61724e05b229309e7a22129e858733ff03703d53
sadmoment@foxmail.com <sadmoment@foxmail.com>
2017-12-28 17:35:51 +0800

    岭回归算法案例

---
+++ training/regression/ridgeRegression.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+__coding__ = "utf-8"
+__author__ = " Ng WaiMing "
+
+from numpy import *
+
+
+def ridgeRegress(xMat, yMat, lam=0.2):
+    xTx = xMat * xMat.T
+    denom = xTx * eye(shape(xMat)[1]) * lam
+    if linalg.det(denom) == 0.0:
+        print('This matrix is singular,cannot do inverse')
+        return
+    ws = denom.I * (xMat.T * yMat)
+    return ws
+
+
+def ridgeTest(xArr, yArr):
+    xMat = mat(xArr)
+    yMat = mat(yArr).transpose()
+    yMean = mean(yMat, 0)
+    yMat = yMat - yMean
+    xMeans = mean(xMat, 0)
+    xVar = var(xMat, 0)
+    xMat = (xMat - xMeans) / xVar
+    numTestPts = 30
+    wMat = zeros((numTestPts, shape(xMat)[1]))
+    for i in range(numTestPts):
+        ws = ridgeRegress(xMat, yMat, exp(i - 10))
+        wMat[i, :] = ws.T
+    return wMat
