143da7726522f92a7fa1eae9a09b4ac1f108f1a8
sadmoment@foxmail.com <sadmoment@foxmail.com>
2017-12-28 16:50:03 +0800

    线性回归主方法

--- training/regression/__init__.py
+++ training/regression/__init__.py
@@ -6,11 +6,23 @@ from training.regression import regression as re
 from training.regression import locallyWeightedLinearRegression as lo
 from numpy import *
 
+
+def rssError(yArr, yHatArr):
+    return ((yArr - yHatArr) ** 2).sum()
+
+
 if __name__ == "__main__":
-    xArr, yArr = re.loadDataSet('../../data/regression/ex0.txt')
-    ws = re.standRegres(xArr, yArr)
-    # print('ws: ', ws)
-    # re.plotRegres(xArr, yArr, ws)
-    yHat = lo.lwlrTest(array(xArr), array(xArr), array(yArr), 0.003)
-    # print(yHat)
-    lo.plotLwlr(array(xArr), array(yArr), array(yHat))
+    # xArr, yArr = re.loadDataSet('../../data/regression/ex0.txt')
+    # ws = re.standRegres(xArr, yArr)
+    # # print('ws: ', ws)
+    # # re.plotRegres(xArr, yArr, ws)
+    # yHat = lo.lwlrTest(array(xArr), array(xArr), array(yArr), 0.003)
+    # # print(yHat)
+    # lo.plotLwlr(array(xArr), array(yArr), array(yHat))
+    abX, abY = re.loadDataSet('../../data/regression/abalone.txt')
+    yHat01 = lo.lwlrTest(abX[0:99], abX[0:99], abY[0:99], 0.1)
+    yHat1 = lo.lwlrTest(abX[0:99], abX[0:99], abY[0:99], 1)
+    yHat10 = lo.lwlrTest(abX[0:99], abX[0:99], abY[0:99], 10)
+    print('0.1: ',rssError(abY[0:99], yHat01.transpose()))
+    print('1: ', rssError(abY[0:99], yHat1.transpose()))
+    print('10: ', rssError(abY[0:99], yHat10.transpose()))
