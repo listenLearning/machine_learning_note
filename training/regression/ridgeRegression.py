388da66db6fe75489bcfed8e8d197b8bfa0b0fca
sadmoment@foxmail.com <sadmoment@foxmail.com>
2017-12-28 23:15:04 +0800

    岭回归算法案例

--- training/regression/ridgeRegression.py
+++ training/regression/ridgeRegression.py
@@ -7,16 +7,34 @@ from matplotlib import pyplot as plt
 
 
 def ridgeRegress(xMat, yMat, lam=0.2):
+    '''
+    实现了给定lambda下的岭回归求解,如果没有指定lambda,则默认为0.2
+    :param xMat:
+    :param yMat:
+    :param lam:
+    :return:
+    '''
+    # 构造X^TX矩阵
     xTx = xMat.T * xMat
+    # 使用lambda乘以单位矩阵(可以调用eye来生成)
     denom = xTx * eye(shape(xMat)[1]) * lam
+    # 判断X^TX的行列式是否为零,如果为零,会导致计算逆矩阵的时候出现错误,直接返回
     if linalg.det(denom) == 0.0:
         print('This matrix is singular,cannot do inverse')
         return
+    # 如果矩阵非奇异就计算回归系数并返回
     ws = denom.I * (xMat.T * yMat)
     return ws
 
 
 def ridgeTest(xArr, yArr):
+    '''
+    为了方便使用岭回归和缩减技术,需要对特征做标准化处理
+    具体做法就是所有特征都减去各自的均值并除以方差
+    :param xArr:
+    :param yArr:
+    :return:
+    '''
     xMat = mat(xArr)
     yMat = mat(yArr).transpose()
     yMean = mean(yMat, 0)
@@ -24,10 +42,14 @@ def ridgeTest(xArr, yArr):
     xMeans = mean(xMat, 0)
     xVar = var(xMat, 0)
     xMat = (xMat - xMeans) / xVar
+    # 初始化lambda数目
     numTestPts = 30
     wMat = zeros((numTestPts, shape(xMat)[1]))
+    # 在numTestPts个不同的lambda下调用ridgeRegress函数
+    # 需要注意的是,这里的lambda应以指数级变化,这样可以看出lambda在取非常小的值时和取非常大的值时分别对结果造成的影响
     for i in range(numTestPts):
         ws = ridgeRegress(xMat, yMat, exp(i - 10))
+        # 将所有的回归系数输出到一个矩阵
         wMat[i, :] = ws.T
     return wMat
 
