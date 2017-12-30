a7555b20a70f041c5af676943a4e7bab8dfb28d5
sadmoment@foxmail.com <sadmoment@foxmail.com>
2017-12-30 16:19:38 +0800

    cart算法运行主方法

--- training/ClassificationAndRegressionTrees/__init__.py
+++ training/ClassificationAndRegressionTrees/__init__.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 __coding__ = "utf-8"
 __author__ = " Ng WaiMing "
+from training.ClassificationAndRegressionTrees import regTrees
+from numpy import *
 
 if __name__ == "__main__":
-    print("")
+    testMat = mat(eye(4))
+    # print(testMat)
+    mat0, mat1 = regTrees.binSplitDataSet(testMat, 1, 0.5)
+    # print(mat0)
+    print(mat1)
