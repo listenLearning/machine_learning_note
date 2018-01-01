2137ad62817c91103a5a6f7d95ecb28171bd3e8f
sadmoment@foxmail.com <sadmoment@foxmail.com>
2018-01-01 18:47:56 +0800

    SVM运行主方法

--- training/action/supervised/svm/__init__.py
+++ training/action/supervised/svm/__init__.py
@@ -2,11 +2,10 @@
 __coding__ = "utf-8"
 __author__ = " Ng WaiMing "
 
-from training.svm import svmMLiA, svmMLiAWithKernel, svmMliADigitalRecognition
-from numpy import *
+from training.action.supervised.svm import svmMLiA, svmMliADigitalRecognition
 
 if __name__ == "__main__":
-    # dataArr, labelArr = svmMLiA.loadDataSet('../../data/svm/testSet.txt')
+    # dataArr, labelArr = svmMLiA.loadDataSet('../../../../data/svm/testSet.txt')
     # b, alphas = svmMLiA.smop(array(dataArr), array(labelArr), 0.6, 0.001, 40)
     # # print(b, '\n')
     # # print(alphas[alphas > 0], '\n')
