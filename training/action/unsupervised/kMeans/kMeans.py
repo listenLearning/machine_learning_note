1c9a2636f0b2cb81fc34fcc68d4871dcaad0e80c
sadmoment@foxmail.com <sadmoment@foxmail.com>
2018-01-01 22:24:57 +0800

    k-means算法相关案例

--- training/action/unsupervised/kMeans/kMeans.py
+++ training/action/unsupervised/kMeans/kMeans.py
@@ -1,3 +1,29 @@
 #!/usr/bin/env python
 __coding__ = "utf-8"
 __author__ = "Ng WaiMing"
+
+from numpy import *
+
+
+def loadDataSet(fileName):
+    dataMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = [float(x) for x in curLine]
+        dataMat.append(fltLine)
+    return dataMat
+
+
+def distEclud(vecA, vecB):
+    return sqrt(sum(power(vecA - vecB, 2)))
+
+
+def randCent(dataSet, k):
+    m, n = shape(dataSet)
+    centroids = mat(zeros((k, n)))
+    for j in range(n):
+        minJ = min(dataSet[:, j])
+        rangeJ = float(max(dataSet[:, j]) - minJ)
+        centroids[:, j] = mat(minJ + rangeJ * random.rand(k, 1))
+    return centroids
