0063f61e78349128f37b0fd08dc17eed369cfb04
sadmoment@foxmail.com <sadmoment@foxmail.com>
2018-01-02 23:28:33 +0800

    Apriori算法案例

--- training/action/unsupervised/Apriori/Apriori.py
+++ training/action/unsupervised/Apriori/Apriori.py
@@ -1,3 +1,39 @@
 #!/usr/bin/env python
 __coding__ = "utf-8"
 __author__ = "Ng WaiMing"
+
+from numpy import *
+
+
+def loadDataSet():
+    return [[1, 3, 4], [2, 3, 5], [1, 2, 3, 5], [2, 5]]
+
+
+def createC1(dataSet):
+    C1 = []
+    for transaction in dataSet:
+        for item in transaction:
+            if not [item] in C1:
+                C1.append([item])
+    C1.sort()
+    return [frozenset(x) for x in C1]
+
+
+def scanD(D, Ck, minSupport):
+    ssCnt = {}
+    for tid in D:
+        for can in Ck:
+            if can.issubset(tid):
+                if can not in ssCnt:
+                    ssCnt[can] = 1
+                else:
+                    ssCnt[can] += 1
+    numItems = float(len(D))
+    retList = []
+    supportData = {}
+    for key in ssCnt:
+        support = ssCnt[key] / numItems
+        if support >= minSupport:
+            retList.insert(0, key)
+        supportData[key] = support
+    return retList, supportData
