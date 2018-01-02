ac3ef4aa416345dfc5641405311baefcd4b9d713
sadmoment@foxmail.com <sadmoment@foxmail.com>
2018-01-02 11:48:43 +0800

    k-means算法运行主方法

--- training/action/unsupervised/kMeans/__init__.py
+++ training/action/unsupervised/kMeans/__init__.py
@@ -7,9 +7,12 @@ from numpy import *
 
 if __name__ == '__main__':
     dataMat = mat(kMeans.loadDataSet('../../../../data/k-means/testSet.txt'))
-    print('min(dataMat[:, 0])', min(dataMat[:, 0]), '\n')
-    print('min(dataMat[:, 1])', min(dataMat[:, 1]), '\n')
-    print('max(dataMat[:, 0])', max(dataMat[:, 0]), '\n')
-    print('max(dataMat[:, 1])', max(dataMat[:, 1]), '\n')
-    print(kMeans.randCent(dataMat, 2),'\n')
-    print(kMeans.distEclud(dataMat[0],dataMat[1]))
+    # print('min(dataMat[:, 0])', min(dataMat[:, 0]), '\n')
+    # print('min(dataMat[:, 1])', min(dataMat[:, 1]), '\n')
+    # print('max(dataMat[:, 0])', max(dataMat[:, 0]), '\n')
+    # print('max(dataMat[:, 1])', max(dataMat[:, 1]), '\n')
+    # print(kMeans.randCent(dataMat, 2),'\n')
+    # print(kMeans.distEclud(dataMat[0],dataMat[1]))
+    centroids, clusterAssment = kMeans.kMeans(dataMat, 4)
+    print('centroids:\n', centroids, '\n')
+    print('clusterAssment:\n',clusterAssment, '\n')
