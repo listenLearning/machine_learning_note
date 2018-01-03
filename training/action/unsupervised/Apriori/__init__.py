9c255ed28feb10a00964a9a7469abafece68807f
sadmoment@foxmail.com <sadmoment@foxmail.com>
2018-01-03 22:00:26 +0800

    Apriori算法运行主方法

--- training/action/unsupervised/Apriori/__init__.py
+++ training/action/unsupervised/Apriori/__init__.py
@@ -13,7 +13,15 @@ if __name__ == '__main__':
     # L1, suppData0 = Apriori.scanD(D, C1, 0.5)
     # print('L1: \n', L1, '\n')
     # print('suppData0: \n', suppData0, '\n')
-    L, supportData = Apriori.apriori(dataSet)
+    # L, supportData = Apriori.apriori(dataSet)
     # print(Apriori.aprioriGen(L[0],2))
-    rules = Apriori.generateRules(L, supportData, minConf=0.7)
-    print('rules: ',rules,'\n')
+    # rules = Apriori.generateRules(L, supportData, minConf=0.7)
+    # print('rules: ',rules,'\n')
+    mushDatSet = [line.split() for line in open('../../../../data/Apriori/mushroom.dat').readlines()]
+    # print(mushDatSet)
+    L, supportData = Apriori.apriori(mushDatSet, minSupport=0.3)
+    # for item in L[1]:
+    #     if item.intersection('2'): print(item)
+
+    for item in L[3]:
+        if item.intersection('2'): print(item)
