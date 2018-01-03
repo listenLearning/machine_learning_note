fef6a9fc19e7739566fcce2e594055b6a45cbfff
sadmoment <sadmoment@foxmail.com>
2018-01-03 12:21:25 +0800

    Apriori算法运行主方法

--- training/action/unsupervised/Apriori/__init__.py
+++ training/action/unsupervised/Apriori/__init__.py
@@ -5,11 +5,13 @@ from training.action.unsupervised.Apriori import Apriori
 
 if __name__ == '__main__':
     dataSet = Apriori.loadDataSet()
-    print('dataSet: \n', dataSet, '\n')
-    C1 = Apriori.createC1(dataSet)
-    print('C1: \n', C1, '\n')
-    D = [set(x) for x in dataSet]
-    print('D: \n', D, '\n')
-    L1, suppData0 = Apriori.scanD(D, C1, 0.5)
-    print('L1: \n', L1, '\n')
-    print('suppData0: \n', suppData0, '\n')
+    # print('dataSet: \n', dataSet, '\n')
+    # C1 = Apriori.createC1(dataSet)
+    # print('C1: \n', C1, '\n')
+    # D = [set(x) for x in dataSet]
+    # print('D: \n', D, '\n')
+    # L1, suppData0 = Apriori.scanD(D, C1, 0.5)
+    # print('L1: \n', L1, '\n')
+    # print('suppData0: \n', suppData0, '\n')
+    L,supportData =Apriori.apriori(dataSet)
+    print(L)
