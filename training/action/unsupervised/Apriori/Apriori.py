a381520d2d24691b42df67c09fcd9046ce9970cd
sadmoment <sadmoment@foxmail.com>
2018-01-03 12:21:03 +0800

    Apriori算法案例

--- training/action/unsupervised/Apriori/Apriori.py
+++ training/action/unsupervised/Apriori/Apriori.py
@@ -73,3 +73,32 @@ def scanD(D, Ck, minSupport):
         # 存储所有的候选项(key)和对应的支持度(support)
         supportData[key] = support
     return retList, supportData
+
+
+def aprioriGen(Lk, k):
+    retList = []
+    lenLk = len(Lk)
+    for i in range(lenLk):
+        for j in range(i + 1, lenLk):
+            L1 = list(Lk[i])[: k - 2]
+            L2 = list(Lk[j])[: k - 2]
+            L1.sort()
+            L2.sort()
+            if L1 == L2:
+                retList.append(Lk[i] | Lk[j])
+    return retList
+
+
+def apriori(dataSet, minSupport=0.5):
+    C1 = createC1(dataSet)
+    D = [set(x) for x in dataSet]
+    L1, supportData = scanD(D, C1, minSupport)
+    L = [L1]
+    k = 2
+    while (len(L[k - 2]) > 0):
+        Ck = aprioriGen(L[k - 2], k)
+        Lk, supK = scanD(D, Ck, minSupport)
+        supportData.update(supK)
+        L.append(Lk)
+        k += 1
+    return L, supportData
