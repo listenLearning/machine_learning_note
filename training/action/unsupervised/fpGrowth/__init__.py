5ae617a7b7ce93808d9e839b7a094f4ad77234a5
sadmoment <sadmoment@foxmail.com>
2018-01-04 12:47:33 +0800

    FP-Growth算法运行主方法

--- training/action/unsupervised/fpGrowth/__init__.py
+++ training/action/unsupervised/fpGrowth/__init__.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python
 __coding__ = "utf-8"
 __author__ = "Ng WaiMing"
+
+from training.action.unsupervised.fpGrowth import fpGrowth
+from numpy import *
+
+if __name__ == '__main__':
+    path = '../../../../data/fpGrowth/kosarak.dat'
+    # rootNode = fpGrowth.treeNode('pyrmid', 9, None)
+    # rootNode.children['eye'] = fpGrowth.treeNode('eye', 13, None)
+    # rootNode.disp()
+    # rootNode.children['phoenix'] = fpGrowth.treeNode('phoenix', 3, None)
+    # rootNode.disp()
+    simpDat = fpGrowth.loadSimpDat()
+    # print(simpDat)
+    initSet = fpGrowth.createInitSet(simpDat)
+    # print(initSet)
+    myFPtree, myHeaderTab = fpGrowth.createTree(initSet, 3)
+    myFPtree.disp()
