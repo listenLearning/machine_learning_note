6f0f0feb24a3ec32d083b4123647e570a30a8664
sadmoment <sadmoment@foxmail.com>
2018-01-05 10:58:54 +0800

    FP-Growth算法运行主方法

--- training/action/unsupervised/fpGrowth/__init__.py
+++ training/action/unsupervised/fpGrowth/__init__.py
@@ -17,6 +17,13 @@ if __name__ == '__main__':
     myFPtree, myHeaderTab = fpGrowth.createTree(initSet, 3)
     # myFPtree.disp()
     # print(fpGrowth.findPrefixPath('x',myHeaderTab['x'][1]))
-    freqItem = []
+    # freqItem = []
     # fpGrowth.mineTree(myFPtree, myHeaderTab, 3, set([]), freqItem)
-    fpGrowth.sort(myHeaderTab)
+    # print('freqItem:\t',freqItem,'\n')
+    parsedDat = [line.strip().split() for line in open(path).readlines()]
+    initSet = fpGrowth.createInitSet(parsedDat)
+    myFPtree, myHeaderTab = fpGrowth.createTree(initSet,100000)
+    myFreqList = []
+    fpGrowth.mineTree(myFPtree,myHeaderTab,100000,set([]),myFreqList)
+    print('myFreqList length:\t',len(myFreqList),'\n')
+    print('myFreqList:\t',myFreqList,'\n')
