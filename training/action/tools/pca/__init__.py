be7bca3270ae01777ad48b5c66ac61f25fc6694a
sadmoment <sadmoment@foxmail.com>
2018-01-05 17:34:10 +0800

    PCA算法运行主方法

--- training/action/tools/pca/__init__.py
+++ training/action/tools/pca/__init__.py
@@ -2,5 +2,14 @@
 __coding__ = "utf-8"
 __author__ = " Ng WaiMing "
 
+from training.action.tools.pca import pca
+from numpy import *
+
 if __name__ == "__main__":
-    print("")
+    path = '../../../../data/PCA/testSet.txt'
+    # dataMat = pca.loadDataSet(path)
+    # lowDMat, reconMat = pca.pca(dataMat, 2)
+    # # print(shape(lowDMat))
+    # pca.drawingWithMatplot(dataMat, reconMat)
+    path = '../../../../data/PCA/secom.data'
+    pca.replaceNanWithMean(path)
